28705bc69900aba8a06a07ec82d0fec69958c98a
brittcyr <cyrbritt@mit.edu>
2013-09-10 20:15:51 -0400

    email the results of the scrape of the dining feeds

--- menu_reader.py
+++ menu_reader.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
 import feedparser
+import smtplib
 import datetime
 from BeautifulSoup import BeautifulSoup
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 now = datetime.datetime.now().strftime('%a, %d %b %Y')
 
@@ -8,9 +12,11 @@ def get_menu(dining_hall):
     feed = {'Maseeh':398, 'Baker':399}[dining_hall]
     url = 'http://legacy.cafebonappetit.com/rss/menu/' + str(feed)
     week = feedparser.parse(url)
+    today = 0
     for entry in week['entries']:
-        if entry['title'] == now:
-            today = entry['summary']
+        today = entry['summary'] if entry['title'] == now else today
+    if not today:
+        return 'Error getting today\'s menu for ' + dining_hall
 
     today = BeautifulSoup(today)
 
@@ -28,3 +34,34 @@ print 'Maseeh:'
 print get_menu('Maseeh') + '\n'
 print 'Baker:'
 print get_menu('Baker')
+
+
+me = "cyrbritt@mit.edu"
+you = "cyrbritt@mit.edu"
+
+# Create message container - the correct MIME type is multipart/alternative.
+msg = MIMEMultipart('alternative')
+msg['Subject'] = "Dinner Menus"
+msg['From'] = me
+msg['To'] = you
+
+# Create the body of the message (a plain-text and an HTML version).
+text = "Menus"
+html = "<h1>Maseeh</h1>" + get_menu('Maseeh') + "<h1>Baker</h1>" + get_menu('Baker')
+
+# Record the MIME types of both parts - text/plain and text/html.
+part1 = MIMEText(text, 'plain')
+part2 = MIMEText(html, 'html')
+
+# Attach parts into message container.
+# According to RFC 2046, the last part of a multipart message, in this case
+# the HTML message, is best and preferred.
+msg.attach(part1)
+msg.attach(part2)
+
+# Send the message via local SMTP server.
+s = smtplib.SMTP('localhost')
+# sendmail function takes 3 arguments: sender's address, recipient's address
+# and message to send - here it is sent as one string.
+s.sendmail(me, you, msg.as_string())
+s.quit()
